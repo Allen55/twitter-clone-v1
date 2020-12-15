627f09ab9316885e9a33a1c6a948d8754c66a26c
Allen Harper <allenharper@Allens-MacBook-Air.local>
2020-12-14 17:01:02 -0800

    remaining files

--- src/Feed.js
+++ src/Feed.js
@@ -1,27 +1,41 @@
-import React from 'react';
-import Post from './Post.js';
-import TweetBox from './TweetBox.js';
-import './Feed.css';
+import React, { useState, useEffect } from "react";
+import Post from "./Post.js";
+import TweetBox from "./TweetBox.js";
+import "./Feed.css";
+import db from "./firebase";
+import FlipMove from "react-flip-move";
 
 function Feed() {
-    return (
-        <div class="feed">
+  const [posts, setPosts] = useState([]);
 
-          <div className="feed__header">
-            <h2>Home</h2>
-          </div>
-
-          <TweetBox />
-
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          {/* Post */}
-          {/* Post */}
-          {/* Post */}
-        </div>
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
     );
+  }, []);
+  return (
+    <div class="feed">
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+
+      <TweetBox />
+
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            userName={post.userName}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
+    </div>
+  );
 }
 
 export default Feed;
--- src/Post.js
+++ src/Post.js
@@ -1,50 +1,44 @@
-import React from 'react';
-import './Post.css';
-import { Avatar, Button } from '@material-ui/core';
-import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
-import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import React, { forwardRef } from "react";
+import "./Post.css";
+import { Avatar } from "@material-ui/core";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 
-function Post({
-    displayName,
-    userName,
-    verified,
-    timestamp,
-    text,
-    avatar
-}) {
-  return (
-    <div className='post'>
+const Post = forwardRef(
+  (
+    { displayName, userName, verified, timestamp, text, image, avatar }, ref) => {
+    return (
+      <div className="post" ref={ref}>
         <div className="post__avatar">
-            <Avatar src='https://thumbs.dreamstime.com/z/businessman-avatar-man-suit-blue-tie-human-profile-userpic-face-features-web-picture-gentlemen-round-button-85824531.jpg' />
+          <Avatar src={avatar} />
         </div>
         <div className="post__body">
-            <div className="post__header">
-                <div className="post__headerText">
-                    <h3>
-                        Allen Harper
-                        <span className="post__headerSpecial">
-                            <VerifiedUserIcon className="post__badge" />
-                        </span>
-                    </h3>
-                </div>
-                <div className="post__headerDescription">
-                    <p>I challenge you to build a twitter clone with React</p>
-                </div>
+          <div className="post__header">
+            <div className="post__headerText">
+              <h3>
+                {displayName}{' '}
+                <span className="post__headerSpecial">
+                  {verified && <VerifiedUserIcon className="post__badge" />} @{userName}
+                </span>
+              </h3>
             </div>
-            <img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png" alt="" width="400" height="200"/>
-            <div className="post__footer">
-                <ChatBubbleOutlineIcon fontSize="small" />
-                <RepeatIcon fontSize="small" />
-                <FavoriteBorderIcon fontSize="small" />
-                <PublishIcon fontSize="small" />
+            <div className="post__headerDescription">
+              <p>{text}</p>
             </div>
+          </div>
+          <img src={image} alt="" width="400" height="200" />
+          <div className="post__footer">
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <PublishIcon fontSize="small" />
+          </div>
         </div>
-    </div>
-
-  )
-}
-
+      </div>
+    );
+  }
+);
 export default Post;
